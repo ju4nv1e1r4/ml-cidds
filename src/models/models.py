24a71490df633b594007285a508eb7ec18442d77
juanvieira <juanvieira1@yahoo.com>
2025-04-16 20:17:58 -0300

    feat: add schema with pydantic data validation for both --- supervised and unsupervised --- model

---
+++ src/models/models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import Literal
+from datetime import datetime
+
+class SupervisedSessionData(BaseModel):
+    start_session: str
+    end_session: str
+    packets: int
+    bytes: int
+    source_port: float
+    flag: str
+
+class UnsupervisedSessionData(BaseModel):
+    start_session: str
+    end_session: str
+    packets: int
+    bytes: int
+    source_port: float
+    flag: str
+    source_ip_freq: float
+    dest_ip_freq: float
+    network_protocol: Literal["TCP", "UDP", "ICMP"]
