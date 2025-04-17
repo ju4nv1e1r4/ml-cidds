17484433841e4d54b11e8922bf3e7da58317c5e5
juanvieira <juanvieira1@yahoo.com>
2025-04-17 16:38:43 -0300

    refactor: add function to save new input data + inference result from both models;  two datasets will be saved as .csv --- NextStep -> send both .csv directly to bucket

--- src/api.py
+++ src/api.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Literal, Optional
+import pandas as pd
+import os
 from .routes.predict import Model
 from .models.models import SupervisedSessionData, UnsupervisedSessionData
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 class RequestPayload(BaseModel):
     mode: Literal["supervised", "unsupervised"]
@@ -11,23 +16,39 @@ class RequestPayload(BaseModel):
 
 app = FastAPI()
 
+def save_inference_log(data: dict, prediction: int, mode: str):
+    filename = "data/new_data_sup.csv" if mode == "supervised" else "data/new_data_unsup.csv"
+
+    data_with_prediction = data.copy()
+    data_with_prediction["prediction"] = prediction
+
+    df = pd.DataFrame([data_with_prediction])
+
+    file_exists = os.path.exists(filename)
+    df.to_csv(filename, mode="a", header=not file_exists, index=False)
+
 @app.post("/detect_anomaly")
-async def detect_anomaly(payload: RequestPayload):
+def detect_anomaly(payload: RequestPayload):
     try:
         if payload.mode == "supervised":
             if payload.supervised_data is None:
                 raise HTTPException(status_code=400, detail="Missing data for supervised mode.")
             model = Model.load_model(supervised=True)
+            input_data = payload.supervised_data.dict()
             features = Model.build_features_supervised(payload.supervised_data.dict())
             prediction = model.predict([features])
+            save_inference_log(input_data, int(prediction[0]), mode="supervised")
 
         elif payload.mode == "unsupervised":
             if payload.unsupervised_data is None:
                 raise HTTPException(status_code=400, detail="Missing data for unsupervised mode.")
             model = Model.load_model(supervised=False)
+            input_data = payload.unsupervised_data.dict()
             features = Model.build_features_unsupervised(payload.unsupervised_data.dict())
             prediction = model.predict([features])
+            save_inference_log(input_data, int(prediction[0]), mode="unsupervised")
 
+        logging.info(f"Prediction: {prediction[0]}")
         return {"prediction": int(prediction[0])}
 
     except Exception as e:
