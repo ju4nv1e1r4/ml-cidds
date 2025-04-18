e1d62465455221a9a4fa6f46dbce5f3708b3ab5e
juanvieira <juanvieira1@yahoo.com>
2025-04-18 01:40:26 -0300

    feat: add pub to publish message for saving new data (increase latency on API using gcloud pub/sub)

---
+++ workers/pub_new_data.py
@@ -0,0 +1,20 @@
+import os
+import json
+import dotenv
+from google.cloud import pubsub_v1
+
+dotenv.load_dotenv()
+
+publisher = pubsub_v1.PublisherClient()
+
+PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+TOPIC_ID = os.getenv("GCP_PUBSUB_TOPIC")
+
+TOPIC_PATH = publisher.topic_path(PROJECT_ID, TOPIC_ID)
+
+def publish_new_data(data: dict):
+    message_json = json.dumps(data)
+    message_bytes = message_json.encode("utf-8")
+
+    future = publisher.publish(TOPIC_PATH, data=message_bytes)
+    return future
