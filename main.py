b52e6fe6fa3df7f264c2d9e8089400c17d0dec4b
juanvieira <juanvieira1@yahoo.com>
2025-04-18 17:35:21 -0300

    cicd: deploy both services

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("src.api:app", host="0.0.0.0", port=8080, log_level="info", reload=True)
+    uvicorn.run("src.api:app", host="0.0.0.0", port=8080, log_level="info")
